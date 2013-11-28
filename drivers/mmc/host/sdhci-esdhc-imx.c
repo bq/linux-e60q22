fb782921697fbfaa0d1a5d2631f8e7809d9debf1
Heiko Stuebner <heiko@sntech.de>
2013-11-28 14:53:50 +0100

    enable the mmc hosts as wakeup-sources, to let us read status information from userspace

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -939,6 +939,9 @@ static int esdhc_pltfm_init(struct sdhci_host *host, struct sdhci_pltfm_data *pd
 		host->quirks &= ~SDHCI_QUIRK_BROKEN_CARD_DETECTION;
 	}
 
+	/* the mmcs can be wakeup sources */
+	device_set_wakeup_capable(mmc_dev(host->mmc), true);
+
 #ifdef CONFIG_PM_RUNTIME
 	host->mmc->caps |= MMC_CAP_POWER_OFF_CARD;
 #endif
